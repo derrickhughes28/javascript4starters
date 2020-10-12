2d21642990635623c4ad6c5e7f1d5772afa729df
derrick <derrick.hughes28@gmail.com>
2020-10-12 01:18:07 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+//first alert
+alert("JavaScript BOIIIII");
+
+// second aler
+alert("JavaScript BOiiiii")
+ 
+var myVariableDerrick;
+myVariableDerrick = 10;
+
+var myVar2 = "Hello";
+var myVar3 = "Hello again";
+
