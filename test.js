504b5c9d9d92c9902b7f424a5ba367394f674710
derrick <derrick.hughes28@gmail.com>
2020-10-12 08:48:53 -0400

    i forgot to commit changes

--- test.js
+++ test.js
@@ -10,3 +10,18 @@ myVariableDerrick = 10;
 var myVar2 = "Hello";
 var myVar3 = "Hello again";
 
+
+var ilikemeat = true;
+ 
+if (ilikemeat){
+document.write("Here is the meaty menu...")
+
+
+}
+else{
+    document.write("no meaty menu 4 u")
+}
+
+var myString = "i am a fun string";
+
+console.log(myString.indexOf("string"));
